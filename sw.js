3efbdf20a6cb1b93dc917b43ea3c8afaea87ba96
liroyha <131881246+liroyha@users.noreply.github.com>
2026-09-07 18:09:59 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+```javascript
+const CACHE_NAME = "mi-echad-ad-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./app-icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
+```
